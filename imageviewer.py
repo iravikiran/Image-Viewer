0c5bed0afc7bf7b3425a39cf14c34ebd6c6c9bc2
iravikiran <iamravikirank@gmail.com>
2020-12-28 21:08:15 +0530

    Added Colors for Navigation Buttons.

--- imageviewer.py
+++ imageviewer.py
@@ -1,3 +1,24 @@
+#
+#               Python-Tkinter - ImageViewer v0.3
+#
+#   A Simple Python based GUI ImageViewer Application written
+#   with Py-Tkinter.
+#       This code performs following functions:
+#           ->   Navigate Options:
+#                   * Next Image
+#                   * Previous Image
+#           ->   Close Application
+#           ->   Status Bar - Image Count.
+#
+#   Feel free to add more features to the code and create a PR
+#   i'm working on adding more features on the next releases.
+#
+#                   Author : Ravi Kiran
+#                   GitHub : iravikiran
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
 from tkinter import *
 from PIL import ImageTk, Image
 import os
@@ -33,11 +54,11 @@ def next(image_num):
 
 	label.grid_forget()
 	label = Label(image=image_list[image_num-1])
-	button_next = Button(root, text="Next", command=lambda: next(image_num+1))
-	button_prev = Button(root, text="Prev", command=lambda: prev(image_num-1))
+	button_next = Button(root, text="Next >>", command=lambda: next(image_num+1), bg="#29ee55")
+	button_prev = Button(root, text="<< Prev", command=lambda: prev(image_num-1), bg="#ffd30e")
 	
 	if image_num >= len(image_list):
-		button_next = Button(root, text="Next", state=DISABLED)
+		button_next = Button(root, text="Next >>", state=DISABLED, bg="#9ef0b0")
 
 	stats = Label(root, text="Showing {0} of {1} Images.".format(image_num, len(image_list)), bd=1, relief=SUNKEN, anchor=E)
 	stats.grid(row=2, column=0, columnspan=3, sticky=W+E)
@@ -55,11 +76,11 @@ def prev(image_num):
 
 	label.grid_forget()
 	label = Label(image=image_list[image_num-1])
-	button_next = Button(root, text="Next", command=lambda: next(image_num+1))
-	button_prev = Button(root, text="Prev", command=lambda: prev(image_num-1))
+	button_next = Button(root, text="Next >>", command=lambda: next(image_num+1), bg="#29ee55")
+	button_prev = Button(root, text="<< Prev", command=lambda: prev(image_num-1), bg="#ffd30e")
 
 	if image_num == 1:
-		button_prev = Button(root, text="Prev", state=DISABLED)
+		button_prev = Button(root, text="<< Prev", state=DISABLED, bg="#f7e9a7")
 
 	stats = Label(root, text="Showing {0} of {1} Images.".format(image_num, len(image_list)), bd=1, relief=SUNKEN, anchor=E)
 	stats.grid(row=2, column=0, columnspan=3, sticky=W+E)
@@ -69,9 +90,9 @@ def prev(image_num):
 	button_prev.grid(row=1, column=0)
 
 
-button_next = Button(root, text="Next", command=lambda: next(2))
-button_close = Button(root, text="Close", command=root.quit)
-button_prev = Button(root, text="Prev", command=prev, state=DISABLED)
+button_next = Button(root, text="Next >>", command=lambda: next(2), bg="#29ee55")
+button_close = Button(root, text="Close", command=root.quit, bg="#f5673d")
+button_prev = Button(root, text="<< Prev", command=prev, state=DISABLED, bg="#f7e9a7")
 
 button_next.grid(row=1, column=2, padx=10, pady=10)
 button_close.grid(row=1, column=1, padx=10, pady=10)
